68779e013f65eb9882de805625624f0a58b4af9e
Krzysztof Kluczynski <krzysztof.klczynski22@gmail.com>
2023-12-05 21:49:03 +0100

    rewritten tests

--- src/test/java/com/example/springbackend/UserTest.java
+++ src/test/java/com/example/springbackend/UserTest.java
@@ -4,19 +4,26 @@ import com.example.springbackend.User.User;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class UserTest {
 
     @Test
     void getNameCreatedInConstructorTest() {
-        User u = new User(1, "Krzysztof");
+        User u = new User(1, "klkuczyn", "Krzysztof", "Kluczyński", 21);
         assertEquals(1, u.getId());
     }
 
     @Test
     void setNameTest() {
-        User u = new User(1, "Jan");
+        User u = new User(1, "jfilpec", "Jan", "Filipecki", 22);
         u.setName("Kacper");
         assertEquals("Kacper", u.getName());
+    }
+
+    @Test
+    void emptyUserTest() {
+        User u = new User();
+        assertNull(u.getName());
     }
 }
