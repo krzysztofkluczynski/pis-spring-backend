00325d176edf275fcdac7d94be95db0648d9219b
Krzysztof Kluczynski <krzysztof.klczynski22@gmail.com>
2024-01-10 17:24:20 +0100

    basic keycloak auth

---
+++ src/main/java/com/example/springbackend/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+//package com.example.springbackend.config;
+//
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity.cors(Customizer.withDefaults())
+//                //.csrf(csrf -> csrf.ignoringRequestMatchers("/"))
+//                .authorizeHttpRequests(httpRequests -> httpRequests.anyRequest().authenticated())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                //.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+//                .build();
+//    }
+//}
