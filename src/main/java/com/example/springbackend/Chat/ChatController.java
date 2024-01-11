b12288cbdb50c57fbbc75a9e98c2f06d97a2263b
Kacper Murygin <kacper.mur123@gmail.com>
2024-01-11 22:11:55 +0100

    fix Added necessary decorator

--- src/main/java/com/example/springbackend/Chat/ChatController.java
+++ src/main/java/com/example/springbackend/Chat/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ChatController {
 
